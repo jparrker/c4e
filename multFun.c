92778b8e1a6e395b1327906e4f8a291663458537
jparrker <jasonkk.parker@gmail.com>
2024-02-10 09:38:36 -0500

    solution

---
+++ multFun.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() {
+    int mymult();
+    int retval;
+
+    retval = mymult(6,7);
+    printf("Answer: %d\n",retval);
+}
+
+int mymult(a, b) {
+    int c = a * b;
+    return c; 
+}
